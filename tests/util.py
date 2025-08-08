2a74b3826cbef3580af9b4772b645daf3c446763
lucaSartore <lucasar.com8@gmail.com>
2025-08-08 19:10:36 +0200

    organization commit

--- tests/util.py
+++
@@ -1,7 +0,0 @@
-def load_clip_to_cpu():
-    model_url = clip.clip._MODELS["RN50"]
-
-    model_path = clip.clip._download(model_url, "./models")
-    model_pt = pt.jit.load(model_path, map_location="cpu").eval()
-    model = clip.clip.build_model(model_pt.state_dict())
-    return model
